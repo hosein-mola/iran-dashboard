fda7de742078f259a0842ab617f19768914eae94
hodev-dev <hodev.dev@gmail.com>
2025-04-13 21:16:31 +0330

    integrage form studio into project

--- components/DragOverlayWrapper.tsx
+++ components/DragOverlayWrapper.tsx
@@ -1,69 +1,74 @@
-"use client"
+'use client'
 import { Active, DragOverlay, useDndMonitor } from '@dnd-kit/core'
-import { snapCenterToCursor } from '@dnd-kit/modifiers';
+import { snapCenterToCursor } from '@dnd-kit/modifiers'
 import React, { useState } from 'react'
-import SidebarBtnElement from './SidebarBtnElement';
-import { ElementType, FormElements } from './FormElement';
-import useDesigner from './hooks/useDesigner';
-import { cn } from '@/lib/utils';
+import SidebarBtnElement from './SidebarBtnElement'
+import { ElementType, FormElements } from './FormElement'
+import useDesigner from './hooks/useDesigner'
+import { cn } from '@/lib/utils'
 
 function DragOverlayWrapper() {
-    const { elements, draggedItem, setDraggedItem } = useDesigner();
-    useDndMonitor({
-        onDragStart: (event) => {
-            setDraggedItem(event.active);
-        },
-        onDragCancel: (event) => {
-            setDraggedItem(null);
-        },
-        onDragEnd: (event) => {
-            setDraggedItem(null);
-        },
-    })
+  const { elements, draggedItem, setDraggedItem } = useDesigner()
+  useDndMonitor({
+    onDragStart: (event) => {
+      setDraggedItem(event.active)
+    },
+    onDragCancel: () => {
+      setDraggedItem(null)
+    },
+    onDragEnd: () => {
+      setDraggedItem(null)
+    },
+  })
 
-    if (draggedItem == null) return null;
-    let node = <div>No drag overlay</div>;
+  if (draggedItem == null) return null
+  let node = <div>No drag overlay</div>
 
-    const isPage = draggedItem?.data?.current?.type == 'page';
-    if (isPage) {
-        node = <div dir='rtl' className='flex flex-row gap-2 bg-background p-1'  >
-            <div
-                className={cn('flex flex-row gap-2 bg-background p-1 w-full')}
-            >
-                <div className={cn('w-full flex  items-center px-2 border cursor-pointer active:ring-2 ring-foreground h-32 rounded-xl',
-                    "ring-2 ring-foreground"
-                )}>{ }</div>
-            </div>
+  const isPage = draggedItem?.data?.current?.type == 'page'
+
+  if (isPage) {
+    node = (
+      <div dir="rtl" className="bg-background flex flex-row gap-2 p-1">
+        <div className={cn('bg-background flex w-full flex-row gap-2 p-1')}>
+          <div
+            className={cn(
+              'ring-foreground flex h-32 w-full cursor-pointer items-center rounded-xl border px-2 active:ring-2',
+              'ring-foreground ring-2'
+            )}
+          >
+            {}
+          </div>
         </div>
-    }
-    const isSidebarBtnElemnt = draggedItem?.data?.current?.isDesignerBtnElement;
-    if (isSidebarBtnElemnt) {
-        const type = draggedItem.data?.current?.type as ElementType;
-        node = <div className='w-full flex flex-grow  bg-accent border rounded-md min-h-[120px] min-w-[120px] scale-[0.5]    !pointer-events-none  '>
-            <SidebarBtnElement formElement={FormElements?.[type]} />
-        </div>
-    }
-
-    const isDesignerElement = draggedItem?.data?.current?.isDesignerElement;
-    if (isDesignerElement == true) {
-        const id = draggedItem?.data?.current?.id;
-        const element = elements.find(el => el.id == id);
-        if (element == null) {
-            node = <div>element not found</div>
-        } else {
-            const DesignerElementComponent = FormElements?.[element?.type];
-            node = <div className='w-full flex flex-grow  bg-accent border rounded-md min-h-[120px] min-w-[120px] scale-[0.5]    !pointer-events-none  '>
-                <SidebarBtnElement formElement={DesignerElementComponent} />
-            </div>
-        }
-    }
-
-
-    return (
-        <DragOverlay modifiers={[snapCenterToCursor]}>
-            {node}
-        </DragOverlay >
+      </div>
     )
+  }
+  const isSidebarBtnElemnt = draggedItem?.data?.current?.isDesignerBtnElement
+  if (isSidebarBtnElemnt) {
+    const type = draggedItem.data?.current?.type as ElementType
+    node = (
+      <div className="bg-accent !pointer-events-none flex min-h-[120px] w-full min-w-[120px] flex-grow scale-[0.5] rounded-md border">
+        <SidebarBtnElement formElement={FormElements?.[type]} />
+      </div>
+    )
+  }
+
+  const isDesignerElement = draggedItem?.data?.current?.isDesignerElement
+  if (isDesignerElement == true) {
+    const id = draggedItem?.data?.current?.id
+    const element = elements.find((el) => el.id == id)
+    if (element == null) {
+      node = <div>element not found</div>
+    } else {
+      const DesignerElementComponent = FormElements?.[element?.type]
+      node = (
+        <div className="bg-accent !pointer-events-none flex min-h-[120px] w-full min-w-[120px] flex-grow scale-[0.5] rounded-md border">
+          <SidebarBtnElement formElement={DesignerElementComponent} />
+        </div>
+      )
+    }
+  }
+
+  return <DragOverlay modifiers={[snapCenterToCursor]}>{node}</DragOverlay>
 }
 
 export default DragOverlayWrapper
--- components/actions/sidebarOrElementOverPage.ts
+++ components/actions/sidebarOrElementOverPage.ts
@@ -1,15 +1,19 @@
-import { DragOverEvent } from "@dnd-kit/core";
-import { DesignerContextType, PageType } from "../context/DesignerContext";
+import { DragOverEvent } from '@dnd-kit/core'
+import { DesignerContextType, PageType } from '../context/DesignerContext'
 
-export function sidebarOrElementOverPage(event: DragOverEvent, selectedPage: PageType, context: DesignerContextType) {
-    const { setSelectedPage, pages, setActive } = context;
-    const { active, over } = event;
-    const isPage = over?.data?.current?.isPage;
-    const pageId = over?.data?.current?.id;
+export function sidebarOrElementOverPage(
+  event: DragOverEvent,
+  selectedPage: PageType,
+  context: DesignerContextType
+) {
+  const { setSelectedPage, pages } = context
+  const { over, active } = event
+  const isOverPage = over?.data?.current?.type == 'page'
+  const pageId = over?.id
+  const isPanelDragged = active.data.current?.type == 'panel'
+  const overPage: PageType | undefined = pages.find((page) => page.id == pageId)
 
-    const overPage: PageType | undefined = pages.find(page => page.id == pageId);
-
-    if (isPage && overPage !== undefined) {
-        setSelectedPage(overPage);
-    }
+  if (isOverPage && isPanelDragged && overPage !== undefined) {
+    setSelectedPage(overPage)
+  }
 }
